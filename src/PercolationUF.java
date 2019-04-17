e8d8c2acf07ed80498875effc1a0bab42fbbab87
Geoff Gaugler <geoffrey.gaugler@duke.edu>
2019-04-16 21:58:45 -0400

    final code

--- src/PercolationUF.java
+++ src/PercolationUF.java
@@ -10,7 +10,7 @@ public class PercolationUF implements IPercolate {
 		myGrid = new boolean[size][size];
 		myFinder = finder;
 		myFinder.initialize((size * size + 2) );
-		VTOP = size*size;
+		VTOP = size * size;
 		VBOTTOM = size * size + 1;
 		myOpenCount = 0;
 	}
