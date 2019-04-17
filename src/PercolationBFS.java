e914fcbf5d4d8b7bae289e147c9bee91a8c43b76
Geoff Gaugler <geoffrey.gaugler@duke.edu>
2019-04-16 21:41:57 -0400

    update BFS

--- src/PercolationBFS.java
+++ src/PercolationBFS.java
@@ -8,30 +8,30 @@ public class PercolationBFS extends PercolationDFSFast {
 	
 	@Override
 	protected void dfs(int row, int col) {
-		if (! inBounds(row,col) || isFull(row, col) || !isOpen(row, col)) return;
+		if (!inBounds(row,col)) return;
 		myGrid[row][col] = FULL;
 		Queue<Integer> q = new LinkedList<>();
-		q.add(row * myGrid.length + col);
+		q.add((row * myGrid.length) + col);
 		while(q.size() > 0) {
 			int rcord = q.poll()/myGrid.length;
 			int ccord = q.poll()%myGrid.length;
 			
-	    	 if(inBounds(rcord, ccord + 1) && myGrid[rcord][ccord + 1] == OPEN) 
+	    	 if(inBounds(rcord, ccord + 1) && isOpen(rcord, ccord + 1) && !isFull(rcord, ccord +1)) 
 	    	 {
 	    		 myGrid[rcord][ccord + 1] = FULL;
 	    		 q.add(rcord * myGrid.length + ccord + 1);
 	    	 }
-	    	 if(inBounds(rcord, ccord - 1) && myGrid[rcord][ccord - 1] == OPEN) 
+	    	 if(inBounds(rcord, ccord - 1) &&  isOpen(rcord, ccord - 1) && !isFull(rcord, ccord - 1)) 
 	    	 {
 	    		 myGrid[rcord][ccord - 1] = FULL;
 	    		 q.add(rcord  *myGrid.length + ccord - 1);
 	    	 }
-			if(inBounds(rcord + 1, ccord) && myGrid[rcord + 1][ccord] == OPEN) 
+			if(inBounds(rcord + 1, ccord) && isOpen(rcord + 1, ccord) && !isFull(rcord + 1, ccord)) 
 			{
 	    		 myGrid[rcord + 1][ccord] = FULL;
 	    		 q.add((rcord+1)*myGrid.length + ccord);
 	    	 }
-	    	 if(inBounds(rcord - 1, ccord) && myGrid[rcord - 1][ccord] == OPEN) 
+	    	 if(inBounds(rcord - 1, ccord) && isOpen(rcord - 1, ccord) && !isFull(rcord - 1, ccord) ) 
 	    	 {
 	    		 myGrid[rcord - 1][ccord] = FULL;
 	    		 q.add((rcord - 1) * myGrid.length + ccord);
